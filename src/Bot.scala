494b3fe2cef4d879e99ba52c9e6236257b948c72
radekbaranowski <baranowski.radek@gmail.com>
2015-01-08 16:54:43 +0100

    vg

--- src/Bot.scala
+++ src/Bot.scala
@@ -10,6 +10,7 @@ class ControlFunction {
   var n = 0
   var filePath:String = _
   var logFile:FileWriter = _
+  val loggingEnabled = false
 
   def respond(input: String) = {
     n += 1
@@ -25,7 +26,7 @@ class ControlFunction {
         val paramMap = kvPairs.toMap
         var round = paramMap("round").toInt
         filePath = "d:\\botlog\\bot_"+round+".log"
-        logFile = new FileWriter(filePath)
+        if (loggingEnabled) {logFile = new FileWriter(filePath)}
         "Status(text=hello)|Log(text="+filePath+")"
 
       case "React" =>
@@ -35,15 +36,17 @@ class ControlFunction {
         val kvPairs = strPairs.map(a => (a(0), a(1)))
         val paramMap = kvPairs.toMap
         val energy = paramMap("energy").toInt
-        logFile.append(paramMap("time").toCharArray)
-        logFile.append('\n')
-        logFile.append(paramMap("view").toString)
-        logFile.append('\n')
+        if (loggingEnabled) {
+          logFile.append(paramMap("time").toCharArray)
+          logFile.append('\n')
+          logFile.append(paramMap("view").toString)
+          logFile.append('\n')
+        }
 
         "Status(text=Round:" + filePath + ")"
 
       case "Goodbye" =>
-      logFile.close()
+        if (loggingEnabled) {logFile.close()}
 
     }
 
