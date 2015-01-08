46fb0cabd9956d66b795c1635ee9729864ff02c6
radekbaranowski <baranowski.radek@gmail.com>
2015-01-08 15:20:27 +0100

    initial commit

---
+++ src/Bot.scala
@@ -0,0 +1,53 @@
+package scalatron.botwar.botPlugin.baranBot
+
+import java.io.FileWriter
+
+class ControlFunctionFactory {
+  def create = new ControlFunction().respond _
+}
+
+class ControlFunction {
+  var n = 0
+  var filePath:String = _
+  var logFile:FileWriter = _
+
+  def respond(input: String) = {
+    n += 1
+    val tokens = input.split('(')
+    val opcode = tokens(0)
+
+    opcode match {
+      case "Welcome" =>
+        val rest = tokens(1).dropRight(1)
+        val params = rest.split(',')
+        val strPairs = params.map(s => s.split('='))
+        val kvPairs = strPairs.map(a => (a(0), a(1)))
+        val paramMap = kvPairs.toMap
+        var round = paramMap("round").toInt
+        filePath = "d:\\botlog\\bot_"+round+".log"
+        logFile = new FileWriter(filePath)
+        "Status(text=hello)|Log(text="+filePath+")"
+
+      case "React" =>
+        val rest = tokens(1).dropRight(1)
+        val params = rest.split(',')
+        val strPairs = params.map(s => s.split('='))
+        val kvPairs = strPairs.map(a => (a(0), a(1)))
+        val paramMap = kvPairs.toMap
+        val energy = paramMap("energy").toInt
+        logFile.append(paramMap("time").toCharArray)
+        logFile.append('\n')
+        logFile.append(paramMap("view").toString)
+        logFile.append('\n')
+
+        "Status(text=Round:" + filePath + ")"
+
+      case "Goodbye" =>
+      logFile.close()
+
+    }
+
+  }
+}
+
+
